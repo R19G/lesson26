feea1981289217d29cb38c0f6c699ce03c6181f3
R19G <guseynov_rustam2017@mail.ru>
2024-12-17 21:06:11 +0300

    Update module7_3.py

--- module7_3.py
+++ module7_3.py
@@ -1,38 +1,41 @@
 class WordsFinder:
 
-    def __init__(self, file_names):
-        self.file_names = file_names
+    def __init__(self, *file_names):
+        self.file_names = list(file_names)
 
     def get_all_words(self):
-        d = ''
-        all_words = {}
-        punctuations = [',', '.', '=', '!', '?', ';', ':', ' - ']
-        with open(self.file_names, 'r', encoding='utf-8') as f:
-            for i in f:
-                i = i.lower()
-                for j in i:
-                    if j in punctuations:
-                        i = i.replace(i, '')
-                d = d + i
-            all_words.update({self.file_names: d.split()})
+
+        all_words = dict()
+        list_of_punctuations = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        list_of_strings = []
+        for file in self.file_names:
+            with open(file, encoding='utf-8') as f_number:
+                for i in f_number:
+                    i = i.lower()
+                    for symbol in list_of_punctuations:
+                        i = i.replace(symbol, '' if symbol != ' - ' else ' ')
+                    list_of_strings.extend(i.split())
+                all_words[file] = list_of_strings
+                list_of_strings = []
         return all_words
 
-    def find(self, txt):
-        dist = {}
-        word = self.get_all_words()[self.file_names]
-        for i in range(len(word)):
-            if txt.lower() == word[i]:
-                dist.update({self.file_names: i+1})
-                return dist
+    def find(self, word):
+        word = word.lower()
+        dictionary_1 = {}
+        for file_name, words in self.get_all_words().items():
+            if word in words:
+                dictionary_1[file_name] = words.index(word) + 1
+        return dictionary_1
 
-    def count(self, txt):
-        dist = {}
-        world = self.get_all_words()[self.file_names]
-        dist.update({self.file_names: world.count(txt.lower())})
-        return dist
+    def count(self, word):
+        word = word.lower()
+        result = {}
+        for file_name, words in self.get_all_words().items():
+            result[file_name] = words.count(word)
+        return result
 
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words())
-print(finder2.find('TEXT'))
-print(finder2.count('teXT'))
+print(finder2.get_all_words()) 
+print(finder2.find('TEXT')) 
+print(finder2.count('teXT')) 
